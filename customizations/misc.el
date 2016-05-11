cbeca630918f7bbff9088d4d1027fc86427bb43e
MR027750 <mike.rodriguez@cerner.com>
2016-05-11 07:44:31 -0500

    Upgrading cider and clojure-mode

--- customizations/misc.el
+++ customizations/misc.el
@@ -22,9 +22,40 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Desktop mode TODO
-;;(desktop-save-mode 1)
+(desktop-save-mode 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Global key bindings
 
 (global-set-key (kbd "M-~") 'other-frame)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Custom cider config
+
+;;(setq nrepl-buffer-name-show-port t)
+;;(setq nrepl-default-port 4005)
+;;(setq cider-known-endpoints '(("maven nrepl" "4005")))
+(setq cider-auto-select-error-buffer nil)
+(setq cider-show-error-buffer nil)
+
+;; Enable paredit for Clojure
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+;; This is useful for working with camel-case tokens, like names of
+;; Java classes (e.g. JavaClassName)
+(add-hook 'clojure-mode-hook 'subword-mode)
+
+;; A little more syntax highlighting
+(require 'clojure-mode-extra-font-locking)
+
+;; Wrap when navigating history.
+(setq cider-repl-wrap-history t)
+
+;; enable paredit in your REPL
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+;; Use clojure mode for other extensions
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
